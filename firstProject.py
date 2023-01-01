775492a9c695b1887af513e20673186e979e6775
ilyurek <76149506+ilyurek@users.noreply.github.com>
2023-01-01 12:33:44 +0300

    Update firstProject.py

--- firstProject.py
+++ firstProject.py
@@ -1,2 +1,16 @@
+# Hello World
 print("hello world")
-print("hello world 2")
+# Integer - Float
+print(56 / 7)  # equals 8
+print(54.25 / 7.1)  # 7.64
+# Variables
+a = 10
+b = 3
+c = a + b
+print(c)
+a, b = b, a  # relocate
+print(a, b)
+print(a)
+i = 5
+i +=1
+#14
