0665fa4dbf452db76b6b67c944aa9b68365bf2c2
ilyurek <76149506+ilyurek@users.noreply.github.com>
2023-01-02 10:51:12 +0300

    Update firstProject.py

--- firstProject.py
+++ firstProject.py
@@ -1,5 +1,5 @@
 # Hello World
-print("hello world")
+print('Remember the Pyhton')
 # Integer - Float
 print(56 / 7)  # equals 8
 print(54.25 / 7.1)  # 7.64
@@ -12,5 +12,21 @@ a, b = b, a  # relocate
 print(a, b)
 print(a)
 i = 5
-i +=1
-#14
+i += 1
+# Mathematics Operators
+print(345 // 7)  # integer division
+print(13 % 4)  # remainder
+print(4 ** 3)  # exponent
+print(4 ** 0.5)  # sqrt
+#  Strings
+print('Ilyurek Kilic')
+x = "Gender"
+print(x[3])  # returns d
+print(x[-3])  # returns d
+print(x[0:3])  # returns Gen
+print(x[::-1])  # reverse
+# Properties of Strings
+print(len(x))  # length
+y = "Man"
+print(x + y)
+# str does not support item assignment
